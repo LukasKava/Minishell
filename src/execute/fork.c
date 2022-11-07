d4b83a47e82348c8f8a2f2d066f59037842a7550
Piotr Biedermann <piotr.biedermann84@gmail.com>
2022-11-07 10:54:45 +0100

    started worn on infile

--- src/execute/fork.c
+++ src/execute/fork.c
@@ -6,11 +6,30 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 11:43:52 by pbiederm          #+#    #+#             */
-/*   Updated: 2022/11/04 13:00:54 by pbiederm         ###   ########.fr       */
+/*   Updated: 2022/11/07 10:28:53 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+// void	exec_handle(t_chunk	*salt, t_info *info, char	**envp)
+// {
+	// switch between the chunks
+// }
+
+void	to_program(t_chunk	*salt, t_info *info, char	**envp)
+{
+	int	fd;
+	int pfd[2];
+
+	fd = open("test.txt", O_RDONLY);
+	if (pipe = pfd[2] < 0)
+		{
+			printf("Error: while creating the pipe.\n");
+			return ;
+		}
+
+}
 
 void	single_child(t_chunk	*salt, t_info *info, char	**envp)
 {
@@ -21,7 +40,7 @@ void	single_child(t_chunk	*salt, t_info *info, char	**envp)
 	{
 		freeing_chunks(salt, info);
 		// printf("terst: %d\n", info->d_quotes);
-		printf("Error: while forking single child process\n");
+		printf("Error: while forking single child process.\n");
 		return ;
 	}
 	if (pid == 0)
