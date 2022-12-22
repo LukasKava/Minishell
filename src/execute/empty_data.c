b7b81f50018772c01f62d16cbd6167451fb3188a
piotr biedermann <piotr.biedermann84@gmail.com>
2022-12-22 19:14:04 +0100

    fixed issue with builtin pipe infile

--- src/execute/empty_data.c
+++ src/execute/empty_data.c
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 11:32:53 by pbiederm          #+#    #+#             */
-/*   Updated: 2022/12/22 14:35:29 by pbiederm         ###   ########.fr       */
+/*   Updated: 2022/12/22 19:11:03 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	empty_data_input(t_chunk	**salt, t_vars *vars)
 void	empty_data_output(t_chunk	**salt, t_vars *vars)
 {
 	t_chunk	*element;
-	int		empty_output_fd[2];
+	int		empty_output_fd;
 
 	element = *salt;
 	if (vars->pipe_group != vars->num_cmd - 1 && \
@@ -46,13 +46,14 @@ void	empty_data_output(t_chunk	**salt, t_vars *vars)
 	(!(pipe_this_node(&element))) && \
 	element->indentifier != ELSE_BLOCK)
 	{
-		if (pipe(empty_output_fd) == -1)
+		empty_output_fd = open("./includes/tmp_out.txt", \
+		O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (empty_output_fd == -1)
 		{
 			write(2, "Error while opening temporary output file\n", 43);
 			g_errors.g_exit_status = 1;
 		}
-		dup2(empty_output_fd[1], STDOUT_FILENO);
-		close(empty_output_fd[1]);
-		close(empty_output_fd[0]);
+		dup2(empty_output_fd, STDOUT_FILENO);
+		close(empty_output_fd);
 	}
 }
